e1caa9ef4143d1c8d287d175ffb92b246dc76401
choonkeat <choonkeat@gmail.com>
2015-07-26 22:41:07 +0800

    Basic app with `Users#show` as login mechanism Once "logged in", user can create and manage `user.posts`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= begin
+      session[:user_id].present? && User.find_by(id: session[:user_id])
+    end
+  end
+  helper_method :current_user
+
 end
